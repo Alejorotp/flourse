63dca7441840b8825a390ba2a3ae8356b47595f7
amyepes <amyepes@uninorte.edu.co>
2025-10-05 22:52:20 -0500

    reports v2
    missing the controller and some other parts tho

--- lib/features/reports/data/datasources/i_report_source.dart
+++ lib/features/reports/data/datasources/i_report_source.dart
@@ -2,15 +2,7 @@ import 'package:flourse/features/reports/domain/models/reports.dart';
 
 abstract class IReportsSource {
   Future<List<Report>> getAllReports({required String courseId});
-
-  Future<Report> createReport({
-    required String courseId,
-    required String title,
-    required String description,
-    required String categoryId,
-    required String visibility,
-  });
-
+  
   Future<void> deleteReport(String id);
 
   Future<List<Report>> getReportsByUserId(String userId);
